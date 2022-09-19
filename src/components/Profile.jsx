5d24998e48f739bf822648ec29328e04a48c6acd
Alan Anselmo <alan.anselmo@olxbr.com>
2022-09-19 18:03:16 -0300

    Clearing localStorage on logout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import {
-  MenuTrigger,
-  ActionButton,
-  Menu,
-  Item
-} from '@adobe/react-spectrum';
+import React from 'react';
+import { MenuTrigger, ActionButton, Menu, Item } from '@adobe/react-spectrum';
 import User from '@spectrum-icons/workflow/User';
 import Light from '@spectrum-icons/workflow/Light';
 import Moon from '@spectrum-icons/workflow/Moon';
-import { observer } from "mobx-react-lite";
-import { useMsal } from "@azure/msal-react";
-import { useStores } from "../stores";
+import { observer } from 'mobx-react-lite';
+import { useMsal } from '@azure/msal-react';
+import { useStores } from '../stores';
+import { clearAutoSave } from '../helpers';
 import './Profile.css';
 
 export const Profile = observer(() => {
@@ -18,20 +14,21 @@ export const Profile = observer(() => {
   const { uiStore } = useStores();
 
   const logout = () => {
-    instance.logoutRedirect({ postLogoutRedirectUri: "/" });
-  }
+    clearAutoSave();
+    instance.logoutRedirect({ postLogoutRedirectUri: '/' });
+  };
 
   const handleKey = (key) => {
-    if (key === "sign-out") {
+    if (key === 'sign-out') {
       logout();
     }
-  }
+  };
 
   const toggleColorScheme = () => {
-    uiStore.colorScheme === "light"
-      ? uiStore.setColorScheme("dark")
-      : uiStore.setColorScheme("light");
-  }
+    uiStore.colorScheme === 'light'
+      ? uiStore.setColorScheme('dark')
+      : uiStore.setColorScheme('light');
+  };
 
   return (
     <div className="osm-profile">
@@ -45,7 +42,7 @@ export const Profile = observer(() => {
         </Menu>
       </MenuTrigger>
       <ActionButton marginStart="size-100" onPress={toggleColorScheme}>
-        {uiStore.colorScheme === "light" ? <Moon /> : <Light />}
+        {uiStore.colorScheme === 'light' ? <Moon /> : <Light />}
       </ActionButton>
     </div>
   );
--- src/helpers/autoSave.js
+++ src/helpers/autoSave.js
@@ -1,14 +1,22 @@
 import { autorun, set, toJS } from 'mobx';
 
 export const autoSave = (_this, name) => {
-	const storedJson = localStorage.getItem(name);
+  const storedJson = localStorage.getItem(name);
 
   if (storedJson) {
-		set(_this, JSON.parse(storedJson));
-	}
+    set(_this, JSON.parse(storedJson));
+  }
 
   autorun(() => {
-		const value = toJS(_this);
-		localStorage.setItem(name, JSON.stringify(value));
-	});
+    const value = toJS(_this);
+    localStorage.setItem(name, JSON.stringify(value));
+  });
+};
+
+export const clearAutoSave = () => {
+  for (let key of Object.keys(localStorage)) {
+    if (key.startsWith('osm_') && key !== 'osm_UIStore') {
+      localStorage.removeItem(key);
+    }
+  }
 };
