1e104783aa0c76d302dcfe6cce297171272dbab0
Alan Anselmo <alan.anselmo@olxbr.com>
2022-09-19 17:53:34 -0300

    New and modified properties in config.template.js

--- src/config.template.js
+++ src/config.template.js
@@ -1,18 +1,18 @@
-import { LogLevel } from "@azure/msal-browser";
+import { LogLevel } from '@azure/msal-browser';
 
 /**
-* Configuration object to be passed to MSAL instance on creation.
-* For a full list of MSAL.js configuration parameters, visit:
-* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
-*/
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
 export const msalConfig = {
   auth: {
-    clientId: "Enter_the_Application_Id_Here",
-    authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-    redirectUri: "Enter_the_Redirect_Uri_Here"
+    clientId: 'Enter_the_Application_Id_Here',
+    authority: 'Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here',
+    redirectUri: 'Enter_the_Redirect_Uri_Here',
   },
   cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    cacheLocation: 'sessionStorage', // This configures where your cache will be stored
     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
   },
   system: {
@@ -34,36 +34,41 @@ export const msalConfig = {
           case LogLevel.Warning:
             console.warn(message);
             return;
+          default:
+            console.log(message);
+            return;
         }
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 /**
-* Scopes you add here will be prompted for user consent during sign-in.
-* By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
-* For more information about OIDC scopes, visit:
-* https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
-*/
-export const loginRequest = {
-  scopes: ["User.Read"]
-};
-
-/**
-* Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
-* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
-*/
-export const graphConfig = {
-  graphMeEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me"
-};
-
-/**
- * OSM Config Object
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
-export const osmConfig = {
-  "apiUrl": "OSM_API_Gateway_URL",
-  "awsAccounts": [
-    { "name": "Account 01", "id": "999999" }
-  ]
+export const loginRequest = {
+  scopes: ['User.Read'],
 };
+
+/**
+ * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const graphConfig = {
+  graphMeEndpoint: 'Enter_the_Graph_Endpoint_Herev1.0/me',
+};
+
+/**
+ * Main OSM API URL
+ */
+export const osmApiUrl = 'OSM_API_Gateway_URL';
+
+/**
+ * List here (awsAccounts) all AWS Accounts you want to manage
+ */
+export const awsAccounts = [{ name: 'Account Example', id: '999999' }];
+
+export const accountList = [{ name: 'All Accounts', id: 'allAccounts' }, ...awsAccounts];
